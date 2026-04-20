d58ab513cb4334c7d6b039785854e1b515d39397
staceyampaire3 <staceyampaire3@gmail.com>
2026-04-20 11:16:59 +0300

    LevelSelection Locked and unlocked

--- Assets/Scripts/ProgressManager.cs
+++ Assets/Scripts/ProgressManager.cs
@@ -59,4 +59,8 @@ public class ProgressManager : MonoBehaviour
     Debug.Log("Progress reset");
     //to reset for lecturer: PlayerPrefs.DeleteAll();
 }
+public bool IsLevelUnlocked(int requiredDew)
+{
+    return totalHealingDew >= requiredDew;
+}
 }
--- Assets/Scripts/UI/LevelSelectionUI.cs
+++ Assets/Scripts/UI/LevelSelectionUI.cs
@@ -5,17 +5,34 @@ public class LevelSelectionUI : MonoBehaviour
 {
     public void LoadFoodRun()
     {
+        // Level 1 always unlocked
         SceneManager.LoadScene("RunnerLevel");
     }
 
     public void LoadExerciseRun()
     {
-        SceneManager.LoadScene("RunnerLevel");
+        // Need 100 Healing Dew
+        if (ProgressManager.instance.totalHealingDew >= 100)
+        {
+            SceneManager.LoadScene("RunnerLevel");
+        }
+        else
+        {
+            Debug.Log("Exercise Run Locked! Need 100 Healing Dew.");
+        }
     }
 
     public void LoadSugarRun()
     {
-        SceneManager.LoadScene("RunnerLevel");
+        // Need 250 Healing Dew
+        if (ProgressManager.instance.totalHealingDew >= 250)
+        {
+            SceneManager.LoadScene("RunnerLevel");
+        }
+        else
+        {
+            Debug.Log("Sugar Run Locked! Need 250 Healing Dew.");
+        }
     }
 
     public void BackToHub()
